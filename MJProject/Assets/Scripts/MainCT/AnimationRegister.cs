151e82e4bc641ba64306fac00c999515b5ea699d
StarFash <1054463026@qq.com>
2017-02-23 18:46:40 +0800

    jxy-170223

---
+++ MJProject/Assets/Scripts/MainCT/AnimationRegister.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using XLua;
+
+[LuaCallCSharp]
+public class AnimationRegister : MonoBehaviour {
+    [CSharpCallLua]
+    public delegate void VoidDelegate();
+    public VoidDelegate animationEvent;
+	// Use this for initialization
+	void Start () {
+        //gameObject.GetComponent<Animation>().clip.Play("GaiDown");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void animationListener() {
+        if (animationEvent != null)
+            animationEvent();
+    }
+}
