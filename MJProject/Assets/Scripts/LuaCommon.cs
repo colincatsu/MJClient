05d60efa4005bbd7aed2a103eb93fc4c1607802e
StarFash <1054463026@qq.com>
2017-02-23 23:46:20 +0800

    jxy_170223

--- MJProject/Assets/Scripts/LuaCommon.cs
+++ MJProject/Assets/Scripts/LuaCommon.cs
@@ -34,7 +34,7 @@ public class LuaCommon
 	public static string xxxtdr2filepath = Application.streamingAssetsPath + "/testxxx2.tdr";
 	public static bool android_platform = true;
 #elif   UNITY_EDITOR
-    public static string resultPath = Application.dataPath + "/MX/";
+    public static string resultPath = Application.dataPath + "/LuaScripts/Resources";
 	public static string xxxtdrfilepath = Application.dataPath + "/StreamingAssets" + "/testxxx.tdr";
 	public static string xxxtdr2filepath = Application.dataPath + "/StreamingAssets" + "/testxxx2.tdr";
 	public static bool android_platform = false;
