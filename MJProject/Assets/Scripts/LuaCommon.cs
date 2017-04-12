9ae70f2a3af3db16680186a0de29666c16a18152
StarFash <1054463026@qq.com>
2017-04-13 00:00:23 +0800

    jxy_FairyGUI

--- MJProject/Assets/Scripts/LuaCommon.cs
+++ MJProject/Assets/Scripts/LuaCommon.cs
@@ -3,21 +3,23 @@ using UnityEngine;
 
 using System.Collections.Generic;
 using System;
-public class LuaEnvSingleton  {
-	
-	static private LuaEnv _instance = null;
-	static public LuaEnv Instance
-	{
-		get
-		{
-			if(_instance == null)
-			{
-				_instance = new LuaEnv();
-			}
-			
-			return _instance;
-		}
-	}
+using FairyGUI;
+public class LuaEnvSingleton
+{
+
+    static private LuaEnv _instance = null;
+    static public LuaEnv Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new LuaEnv();
+            }
+
+            return _instance;
+        }
+    }
 }
 
 [LuaCallCSharp]
@@ -43,7 +45,7 @@ public class LuaCommon
 #endif
 
     public static bool IsMacPlatform()
-	{
+    {
 #if UNITY_EDITOR
         string os = System.Environment.OSVersion.ToString();
         if (os.Contains("Unix"))
@@ -57,16 +59,16 @@ public class LuaCommon
 #else
         return false;
 #endif
-	}
+    }
 
-	public static bool IsIOSPlatform()
-	{
+    public static bool IsIOSPlatform()
+    {
 #if UNITY_IOS || UNITY_IPHONE
 		return true;
 #else
-		return false;
+        return false;
 #endif
-	}
+    }
 }
 
 //注意：用户自己代码不建议在这里配置，建议通过标签来声明!!
@@ -81,47 +83,49 @@ public class TestCaseGenConfig : XLua.GenConfig
             return new List<Type>()
             {
                 typeof(UnityEngine.TextAsset),
-//                typeof(EventContext),
-//                typeof(EventDispatcher),
-//                typeof(EventListener),
-//                typeof(InputEvent),
-//_GT(typeof(DisplayObject)),
-//_GT(typeof(Container)),
-//_GT(typeof(Stage)),
-//_GT(typeof(Controller)),
-//_GT(typeof(GObject)),
-//_GT(typeof(GGraph)),
-//_GT(typeof(GGroup)),
-//_GT(typeof(GImage)),
-//_GT(typeof(GLoader)),
-//_GT(typeof(PlayState)),
-//_GT(typeof(GMovieClip)),
-//_GT(typeof(TextFormat)),
-//_GT(typeof(GTextField)),
-//_GT(typeof(GRichTextField)),
-//_GT(typeof(GTextInput)),
-//_GT(typeof(GComponent)),
-//_GT(typeof(GList)),
-//_GT(typeof(GRoot)),
-//_GT(typeof(GLabel)),
-//_GT(typeof(GButton)),
-//_GT(typeof(GComboBox)),
-//_GT(typeof(GProgressBar)),
-//_GT(typeof(GSlider)),
-//_GT(typeof(PopupMenu)),
-//_GT(typeof(ScrollPane)),
-//_GT(typeof(Transition)),
-//_GT(typeof(UIPackage)),
-//_GT(typeof(Window)),
-//_GT(typeof(GObjectPool)),
-//_GT(typeof(Relations)),
-//_GT(typeof(RelationType)),
+                typeof(UIPanel),
+                typeof(EventContext),
+                typeof(EventDispatcher),
+                typeof(EventListener),
+                typeof(InputEvent),
+                typeof(DisplayObject),
+                typeof(Container),
+                typeof(Stage),
+                typeof(Controller),
+                typeof(GObject),
+                typeof(GGraph),
+                typeof(GGroup),
+                typeof(GImage),
+                typeof(GLoader),
+                typeof(PlayState),
+                typeof(GMovieClip),
+                typeof(TextFormat),
+                typeof(GTextField),
+                typeof(GRichTextField),
+                typeof(GTextInput),
+                typeof(GComponent),
+                typeof(GList),
+                typeof(GRoot),
+                typeof(GLabel),
+                typeof(GButton),
+                typeof(GComboBox),
+                typeof(GProgressBar),
+                typeof(GSlider),
+                typeof(PopupMenu),
+                typeof(ScrollPane),
+                typeof(Transition),
+                typeof(UIPackage),
+                typeof(Window),
+                typeof(GObjectPool),
+                typeof(Relations),
+                typeof(RelationType),
             };
         }
     }
 
     //C#静态调用Lua的配置（包括事件的原型），仅可以配delegate，interface
-    public List<Type> CSharpCallLua {
+    public List<Type> CSharpCallLua
+    {
         get
         {
             return null;
