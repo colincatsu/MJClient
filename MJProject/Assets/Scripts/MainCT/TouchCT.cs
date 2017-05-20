1433b0ac1a7a04d4ac8751847769ceb0b82898c3
StarFash <1054463026@qq.com>
2017-05-20 22:53:31 +0800

    jxy_paiPos,clickPai

--- MJProject/Assets/Scripts/MainCT/TouchCT.cs
+++ MJProject/Assets/Scripts/MainCT/TouchCT.cs
@@ -45,8 +45,9 @@ public class TouchCT : MonoBehaviour {
         }
     }
 
-    bool inArea(Vector2 position) {
+    bool inArea(Vector3 position) {
         return (Mathf.Abs(transform.position.x - position.x) < myRenderer.bounds.size.x/2
-                && Mathf.Abs(transform.position.y - position.y) < myRenderer.bounds.size.y/2);
+                && Mathf.Abs(transform.position.y - position.y) < myRenderer.bounds.size.y/ 2
+                && Mathf.Abs(transform.position.z - position.z) < myRenderer.bounds.size.z / 2);
     }
 }
