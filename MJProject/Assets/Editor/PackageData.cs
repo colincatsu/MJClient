3b2dbf01a2becaf2bf70cc688b29bdb99a6ccb51
StarFash <1054463026@qq.com>
2017-06-15 23:58:13 +0800

    jxy_loaderBuging

--- MJProject/Assets/Editor/PackageData.cs
+++ MJProject/Assets/Editor/PackageData.cs
@@ -10,12 +10,18 @@ class PackageData {
     [UnityEditor.MenuItem("Tools/PackageData")]
     public static void PackageAssets()
 	{
-        UPKFolder.PackFolderAsync(LuaCommon.resultPath + "Asset", LuaCommon.resultPath + "Asset.upk", ShowProgress);
+        UPKFolder.PackFolderAsync(LuaCommon.resultPath + "MyAssets", LuaCommon.resultPath + "MyAssets.upk", ShowProgress);
         AssetDatabase.Refresh();
     }
     static void ShowProgress(long all, long now)
     {
         double progress = (double)now / all;
         Debug.Log("当前进度为: " + progress);
+    }
+    [UnityEditor.MenuItem("Tools/UnPackageData")]
+    public static void UnPackageAssets()
+    {
+        UPKFolder.UnPackFolderAsync(Application.persistentDataPath + "/MyAssets.upk", Application.persistentDataPath + "/", ShowProgress);
+        AssetDatabase.Refresh();
     }
 }
