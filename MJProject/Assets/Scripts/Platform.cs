72422a37f4878e072ffb54ae7874dd5a6fcee5b8
jiaxinyu <jiaxinyu@doodlemobile.com>
2017-07-07 19:22:18 +0800

    jxy_texture

--- MJProject/Assets/Scripts/Platform.cs
+++ MJProject/Assets/Scripts/Platform.cs
@@ -8,7 +8,7 @@ using XLua;
 [LuaCallCSharp]
 public class Platform : MonoBehaviour {
     private static Platform _instance;
-
+    public Texture2D myWXPic;
     public static Platform Instance
     {
         get
@@ -62,6 +62,67 @@ public class Platform : MonoBehaviour {
             currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
         }
         Debug.Log("**************Platform Unity InitX:" + currentActivity);
+        if (!Directory.Exists(Application.persistentDataPath + "/ImageCache/"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/ImageCache/");
+        }
+        myWXPic = null;
+    }
+    public void SetAsyncImage(string url)
+    {
+        //开始下载图片前，将UITexture的主图片设置为占位图  
+        if (myWXPic != null)
+        {
+            image.sprite = myWXPic;
+            return;
+        }
+
+        //判断是否是第一次加载这张图片  
+        if (!File.Exists(path + url.GetHashCode()))
+        {
+            //如果之前不存在缓存文件  
+            StartCoroutine(DownloadImage(url));
+        }
+        else
+        {
+            StartCoroutine(LoadLocalImage(url));
+        }
+    }
+
+    IEnumerator DownloadImage(string url)
+    {
+        Debug.Log("downloading new image:" + path + url.GetHashCode());//url转换HD5作为名字  
+        WWW www = new WWW(url);
+        yield return www;
+
+        Texture2D tex2d = www.texture;
+        //将图片保存至缓存路径  
+        byte[] pngData = tex2d.EncodeToPNG();                         //将材质压缩成byte流  
+        File.WriteAllBytes(path + url.GetHashCode(), pngData);        //然后保存到本地  
+
+        myWXPic = tex2d;
+    }
+
+    IEnumerator LoadLocalImage(string url)
+    {
+        string filePath = "file:///" + path + url.GetHashCode();
+
+        Debug.Log("getting local image:" + filePath);
+        WWW www = new WWW(filePath);
+        yield return www;
+
+        Texture2D texture = www.texture;
+        myWXPic = texture;
+    }
+
+    public string path
+    {
+        get
+        {
+            //pc,ios //android :jar:file//  
+            return Application.persistentDataPath + "/ImageCache/";
+
+        }
     }
 
     public void LoginWeChat()
