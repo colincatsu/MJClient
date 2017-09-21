a704a02a98382fce75f6989d0713b5cb4d83b6ea
StarFash <1054463026@qq.com>
2017-09-22 01:00:18 +0800

    jxy_loadingBug

--- MJProject/Assets/Scripts/AssetsHandle.cs
+++ MJProject/Assets/Scripts/AssetsHandle.cs
@@ -7,9 +7,10 @@ using YZL.Compress.Info;
 
 [LuaCallCSharp]
 public class AssetsHandle : MonoBehaviour {
-    public static double progress = 0;
+    public static float progress = 0;
+    private static float progressTemp = 0;
     [CSharpCallLua]
-    public delegate void OnFinish(double progress);
+    public delegate void OnFinish(float progress);
     public static OnFinish dataFinish;
     public static void UnPackAssetFolder(string inpath ,string outpath)
     {
@@ -17,16 +18,23 @@ public class AssetsHandle : MonoBehaviour {
     }
     public static void ShowProgress(long all, long now)
     {
-        progress = (double)now / all;
-        Debug.Log("当前进度为: " + progress);
-        if(dataFinish != null)
+        progress = (float)now / all;
+        //Debug.Log("当前进度为: " + progress);
+        if(dataFinish != null && progress == 1)
         {
+            progressTemp = progress;
+            dataFinish(progress);
+            return;
+        }
+        if(dataFinish != null && progress - progressTemp >= 0.1f)
+        {
+            progressTemp = progress;
             dataFinish(progress);
         }
     }
     public static void CreateNewAsset(byte[] stream,string path)
     {
-        Debug.LogWarning("hhhhh");
+        //Debug.LogWarning("hhhhh");
         FileStream fs = new FileStream(path, FileMode.Create);   //
         fs.Write(stream, 0, stream.Length);
         fs.Flush();
